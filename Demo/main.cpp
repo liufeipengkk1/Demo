53c47c861eb4c5485ed1edb152c2d8a1ed063e5e
feiper <liufeipengkk1@163.com>
2017-09-07 00:23:15 +0800

    [MOD: main]

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <optix_world.h>
 
 using namespace std;
 
 int main() {
 	cout << "liufeipeng" << endl;
-	
+	optix::Matrix4x4 transform = optix::Matrix4x4::translate(optix::make_float3(0));
+	cout << transform << endl;
 	getchar();
 	return 0;
 }
