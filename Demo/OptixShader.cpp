5482f088bc1fb0b6e377dd963195b75983873b97
feiper <liufeipengkk1@163.com>
2017-09-08 03:38:33 +0800

    [MOD: 着色器程序]

--- Demo/OptixShader.cpp
+++ Demo/OptixShader.cpp
@@ -5,7 +5,19 @@ OptixShader::OptixShader()
 {
 }
 
-
 OptixShader::~OptixShader()
 {
+
+}
+
+void OptixShader::setName(const string& name) {
+	m_name = name;
+}
+
+void OptixShader::setShadingFunction(Context context,
+	const string& ptxFile, PTXFunction function) {
+	m_program = context->createProgramFromPTXFile(ptxFile, function);
+}
+Handle<VariableObj> OptixShader::operator[](const std::string& varname) {
+	return m_program[varname];
 }
--- Demo/OptixShader.h
+++ Demo/OptixShader.h
@@ -1,8 +1,31 @@
-#pragma once
+#ifndef OPTIXCORE_OPTIXSHADER_H
+#define OPTIXCORE_OPTIXSHADER_H
+
+#include <optix_world.h>
+#include <iostream>
+
+using namespace std;
+using namespace optix;
+
+typedef string PTXFunction;
+
 class OptixShader
 {
 public:
 	OptixShader();
-	~OptixShader();
+	virtual ~OptixShader();
+	
+	void setShadingFunction(Context context, const string& ptxFile, PTXFunction function);
+	void setName(const string&  name);
+	string getName();
+
+	Handle<VariableObj> operator[](const std::string& varname);
+
+	Program getHandle() { return m_program; }
+
+private:
+	string  m_name;
+	Program m_program;
 };
 
+#endif
