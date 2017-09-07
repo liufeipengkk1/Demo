1938dfc1b98465a7c349331155f843079055f886
feiper <liufeipengkk1@163.com>
2017-09-08 03:38:01 +0800

    [MOD: 材质]

--- Demo/OptixMaterial.cpp
+++ Demo/OptixMaterial.cpp
@@ -1,10 +1,34 @@
 #include "OptixMaterial.h"
+#include "OptixShader.h"
 
 
 OptixMaterial::OptixMaterial()
 {
+	m_material = nullptr;
 }
 
+
+void OptixMaterial::setShader(OptixShaderType type, OptixShader& shader){
+	m_shaders[type] = shader;
+}
+
+void OptixMaterial::linkShader(Context context) {
+	m_material = context->createMaterial();
+	auto& itr1 = m_shaders.find(OPTIX_SHADER);
+	auto& itr2 = m_shaders.find(OPTIX_SHADOW);
+	if (itr1 != m_shaders.end()) {
+		m_material->setClosestHitProgram(Shading_Ray, itr1->second.getHandle());
+	}
+	else {
+		cout << "OptixMaterial: No Shader Program" << endl;
+	}
+	if (itr2 != m_shaders.end()) {
+		m_material->setAnyHitProgram(Shadow_ray, itr2->second.getHandle());
+	}
+	else {
+		cout << "OptixMaterial: No Shadow Program" << endl;
+	}
+}
 
 OptixMaterial::~OptixMaterial()
 {
--- Demo/OptixMaterial.h
+++ Demo/OptixMaterial.h
@@ -1,11 +1,31 @@
 #ifndef OPTIXCORE_OPTIXMATERIAL_H
 #define OPTIXCORE_OPTIXMATERIAL_H
 
+#include <optix_world.h>
+#include <iostream>
+#include <map>
+
+#include "optixcore_base_define.h"
+using namespace std;
+
+class OptixShader;
+
 class OptixMaterial
 {
 public:
 	OptixMaterial();
 	~OptixMaterial();
+
+	void setShader(OptixShaderType type ,OptixShader& shader);
+	void linkShader(optix::Context context);
+	optix::Material getMaterial() { return m_material; }
+	
+	void setName(const string& name) { m_name = name; }
+	string getName() { return m_name; }
+private:
+	optix::Material m_material;
+	string m_name;
+	map<int, OptixShader> m_shaders;
 };
 
 #endif
