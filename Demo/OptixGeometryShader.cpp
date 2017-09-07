3d5512e9305727c344376aa66b9a4b1ac5b2ee42
feiper <liufeipengkk1@163.com>
2017-09-08 03:37:26 +0800

    [MOD: 几何着色器]

---
+++ Demo/OptixGeometryShader.cpp
@@ -0,0 +1,14 @@
+#include "OptixGeometryShader.h"
+
+
+OptixGeometryShader::OptixGeometryShader()
+{
+	m_boundProgram = nullptr;
+	m_intesectProgram = nullptr;
+	m_name = "";
+}
+
+
+OptixGeometryShader::~OptixGeometryShader()
+{
+}
---
+++ Demo/OptixGeometryShader.h
@@ -0,0 +1,33 @@
+#ifndef OptixGeometryShader_H
+#define OptixGeometryShader_H
+#include <iostream>
+
+#include "OptixShader.h"
+#include "optixcore_base_define.h"
+
+class OptixGeometryShader
+{
+public:
+	OptixGeometryShader();
+	~OptixGeometryShader();
+
+	void setBoundShader(OptixShader& shader) {
+		m_boundProgram = shader.getHandle();
+	}
+	void setIntesectShader(OptixShader& shader) {
+		m_intesectProgram = shader.getHandle();
+	}
+
+	Program getBoundProgram() { return m_boundProgram; }
+	Program getIntesectProgram() { return m_intesectProgram; }
+
+	void setName(const string& name) { m_name = name; }
+	string getName() const { return m_name; };
+
+private:
+	Program m_boundProgram;
+	Program m_intesectProgram;
+	std::string m_name;
+};
+
+#endif
