65cd79bcc7fcf4dbe08f0233f3fadf68b6df05d5
feiper <liufeipengkk1@163.com>
2017-09-10 00:28:24 +0800

    [ADD: OptixContex上下文]

---
+++ Demo/OptixContext.cpp
@@ -0,0 +1,49 @@
+#include "OptixContext.h"
+
+
+OptixContext::OptixContext()
+{
+	m_context = Context::create();
+	m_width = 640;
+	m_height = 480;
+	m_enablePrint = true;
+	m_curEntryPoint = 0;
+
+	m_context->setRayTypeCount(RAY_TYPE_COUNT_);
+	m_context->setEntryPointCount(OPTIX_ENTRY_POINT_COUNTS);
+	m_context->setStackSize(OPTIX_STACK_SIZE);
+
+	m_context->setPrintEnabled(m_enablePrint);
+	m_context->setPrintBufferSize(OPTIX_PRINT_BUFFER_SIZE);
+}
+
+
+OptixContext::~OptixContext()
+{
+}
+
+void OptixContext::setExceptionShader(OptixShader* shader) {
+	m_exceptionShader = SharePtr<OptixShader>(shader);
+	m_context->setExceptionProgram(Shading_Ray, m_exceptionShader->getHandle());
+}
+
+void OptixContext::setViewPort(int w, int h) {
+	m_width = w;
+	m_height = h;
+}
+
+Handle<VariableObj> OptixContext::operator[](const string& varStr) {
+	return m_context[varStr];
+}
+
+void OptixContext::setPrintEnable(bool enable) {
+	m_enablePrint = enable;
+	m_context->setPrintEnabled(m_enablePrint);
+}
+
+void OptixContext::setCurEntryPoint(int idx) {
+	Assert(idx > OPTIX_ENTRY_POINT_COUNTS);
+	if (idx < OPTIX_ENTRY_POINT_COUNTS) {
+		m_curEntryPoint = idx;
+	}
+}
---
+++ Demo/OptixContext.h
@@ -0,0 +1,46 @@
+#ifndef OPTIXCORE_OPTIXCONTEXT_
+#define OPTIXCORE_OPTIXCONTEXT_
+
+#include <iostream>
+#include <optix_world.h>
+#include "OptixShader.h"
+#include "Reference.h"
+#include "optixcore_base_define.h"
+#include "SharePtr.h"
+
+using namespace std;
+using namespace optix;
+
+class OptixContext :public Referenced
+{
+public:
+	OptixContext();
+	void setViewPort(int w, int h);
+	void setExceptionShader(OptixShader* shader);
+	void setExceptionEnable(bool enable = true){}
+	void setPrintEnable(bool enable = false);
+	void setCurEntryPoint(int idx);
+
+	int getW() { return m_width; }
+	int getH() { return m_height; }
+	int getCurEntryPoint() { return m_curEntryPoint; }
+
+	Context getContext() { return m_context; }
+
+	~OptixContext();
+
+	void setTopGeometryName(const string& name){ m_topRootName = name;}
+	string getTopGeometryName() { return m_topRootName; }
+	Handle<VariableObj> operator[](const string& varStr);
+
+private:
+	Context m_context;
+	int m_width;
+	int m_height;
+	SharePtr<OptixShader> m_exceptionShader;
+	bool m_enablePrint;
+	int m_curEntryPoint;
+	string m_topRootName;
+};
+
+#endif
